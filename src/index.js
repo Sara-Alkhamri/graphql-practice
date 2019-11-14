0d86a5cf0e35cc07dc3f844d616bc8294bfedcfa
Sara Alkhamri <s.alkhamri@gmail.com>
2019-11-14 12:50:00 -0800

    Initial push

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//import express, express-graphql middleware and schema
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+//Create server
+const server = express();
+
+const dev = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT || 5000
+
+//mounting our schema on '/graphql' route
+server.use('/graphql', graphqlHTTP((req, res, qql) => ({
+
+    schema,
+    graphql: dev,
+    pretty: dev
+
+})))
+
+//starting server
+server.listen(PORT, err => {
+    if(err) throw err 
+    console.log(`Rady on PORT ${PORT}`)
+})
